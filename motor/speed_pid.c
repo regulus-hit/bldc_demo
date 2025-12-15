a1f38be816fd22e438f4130c1b5477898d147a90
ZHANG Yangyu <zyy@yinhe.ht>
2025-12-15 17:59:08 +0800

    USE MACRO NUMBER FOR 2*PI

--- motor/speed_pid.c
+++ motor/speed_pid.c
@@ -44,7 +44,7 @@ void Speed_Pid_Calc(real32_T ref_temp, real32_T fdb_temp, real32_T* out_temp, SP
 	real32_T temp;
 
 	/* Calculate error with unit conversion: Hz -> rad/s (multiply by 2*PI) */
-	error = 6.28318548F * ref_temp - fdb_temp;
+	error = MATH_2PI * ref_temp - fdb_temp;
 
 	/* PI control law: P + I (standard form) */
 	temp = current_pid_temp->P_Gain * error + current_pid_temp->I_Sum;
