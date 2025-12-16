regulus-hit/bldc_demo

commit f18c41da7ac75cea57856ae9a774c3f41e590e57

Move ALL user defined interrupts to main.c

user/main.c
@@ -58,6 +58,52 @@ int main(void)
 	}
 }
 
+/**
+* @brief  This function handles SysTick Handler.
+* @param  None
+* @retval None
+*/
+void SysTick_Handler(void)
+{
+	low_task_c_systick_sub();
+}
+
+/**
+ * @brief TIM1 Update/TIM10 Interrupt Handler
+ * 
+ * Handles PWM timer update events. Currently only clears the flag.
+ * Could be used for PWM cycle synchronization or debugging.
+ */
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+	TIM_ClearFlag(PWM_TIM, TIM_FLAG_Update);
+}
+
+void TIM2_IRQHandler(void)
+{
+	hall_sensor_c_tim2_sub();
+}
+
+void ADC_IRQHandler(void)
+{
+	adc_c_adc_sub();
+}
+
+void DMA1_Stream6_IRQHandler(void)
+{
+	adc_c_dma1_stream6_sub();
+}
+
+/**
+ * @brief USB OTG Full Speed Interrupt Handler
+ * 
+ * Handles USB communication events for PC interface.
+ * Delegates to USB device library interrupt service routine.
+ */
+void OTG_FS_IRQHandler(void)
+{
+	USBD_OTG_ISR_Handler(&USB_OTG_dev);
+}
 
 /**
  * @brief Delay function using SysTick

user/main.h
@@ -65,6 +65,11 @@ typedef enum
 	FALSE = 0, TRUE  = !FALSE
 }
 bool;
+
+void SysTick_Handler(void);
+void TIM1_UP_TIM10_IRQHandler(void);
+void OTG_FS_IRQHandler(void);
+
 void TimingDelay_Decrement(void);
 extern void Delay(__IO uint32_t nTime);
 

user/stm32f4xx_it.c
@@ -136,34 +136,6 @@ void PendSV_Handler(void)
 {
 }
 
-/**
-* @brief  This function handles SysTick Handler.
-* @param  None
-* @retval None
-*/
-
-/**
- * @brief TIM1 Update/TIM10 Interrupt Handler
- * 
- * Handles PWM timer update events. Currently only clears the flag.
- * Could be used for PWM cycle synchronization or debugging.
- */
-void TIM1_UP_TIM10_IRQHandler(void)
-{
-	TIM_ClearFlag(PWM_TIM, TIM_FLAG_Update);
-}
-
-/**
- * @brief USB OTG Full Speed Interrupt Handler
- * 
- * Handles USB communication events for PC interface.
- * Delegates to USB device library interrupt service routine.
- */
-void OTG_FS_IRQHandler(void)
-{
-	USBD_OTG_ISR_Handler(&USB_OTG_dev);
-}
-
 /******************************************************************************/
 /*                 STM32F4xx Peripherals Interrupt Handlers                   */
 /*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */

user/stm32f4xx_it.h
@@ -49,8 +49,7 @@ void UsageFault_Handler(void);
 void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
-void SysTick_Handler(void);
-void OTG_FS_IRQHandler(void);
+
 #ifdef __cplusplus
 }
 #endif

motor/adc.c
@@ -286,7 +286,7 @@ void motor_run(void)
  * This is the highest priority interrupt in the system as it drives FOC control.
  * Timing is synchronized with PWM to sample currents at optimal point in cycle.
  */
-void ADC_IRQHandler(void)
+void adc_c_adc_sub(void)
 {
 	if ((SAMPLE_ADC->SR & ADC_FLAG_JEOC) == ADC_FLAG_JEOC)
 	{
@@ -382,7 +382,7 @@ void send_PC(float wave1, float wave2, float wave3, float wave4)
  * Called when UART TX DMA transfer completes.
  * Cleans up DMA and allows next transmission.
  */
-void DMA1_Stream6_IRQHandler(void)
+void adc_c_dma1_stream6_sub(void)
 {
 	if (DMA_GetITStatus(DMA1_Stream6, DMA_IT_TCIF6))
 	{

motor/adc.h
@@ -25,4 +25,7 @@ extern uint8_t speed_close_loop_flag;
 extern uint16_t ADC1ConvertedValue[5];
 extern float motor_direction;
 
+void adc_c_adc_sub(void);
+void adc_c_dma1_stream6_sub(void);
+
 #endif

motor/hall_sensor.c
@@ -20,7 +20,7 @@ float hall_speed;
  * The three Hall sensors (A, B, C) create 6 distinct states per electrical cycle,
  * allowing 60-degree resolution position sensing.
  */
-void TIM2_IRQHandler(void)
+void hall_sensor_c_tim2_sub(void)
 {
 	float temp;
 	if (TIM_GetFlagStatus(HALL_TIM, TIM_FLAG_CC1) == SET)

motor/hall_sensor.h
@@ -18,4 +18,6 @@ extern float hall_angle;		/* Rotor angle from Hall sensors */
 extern float hall_angle_add;	/* Angle increment per sample */
 extern float hall_speed;		/* Rotor speed from Hall sensors */
 
+void hall_sensor_c_tim2_sub(void);
+
 #endif

commit 190833ddd0e08b3fccdf16b27801b9878febcae3

[BUGFIX] Fix the problem introduced in Commit f18c41d, where USB related function declarations was not properly moved

user/main.c
@@ -6,8 +6,12 @@
 #include "UpperComputer.h"
 #include "USART2.h"
 
-static __IO uint32_t uwTimingDelay;
+/* Private functions ---------------------------------------------------------*/
+extern USB_OTG_CORE_HANDLE USB_OTG_dev;
+extern uint32_t USBD_OTG_ISR_Handler(USB_OTG_CORE_HANDLE * pdev);
+extern __IO uint32_t data_sent;
 
+static __IO uint32_t uwTimingDelay;
 /**
  * @brief Main Program
  * 

user/main.h
@@ -33,6 +33,7 @@
 #include <math.h>
 
 #include "stm32f4xx.h"
+#include "usbd_cdc_core_loopback.h"
 
 #include "public.h"
 #include "foc_define_parameter.h"

user/stm32f4xx_it.c
@@ -41,9 +41,7 @@
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
-extern USB_OTG_CORE_HANDLE USB_OTG_dev;
-extern uint32_t USBD_OTG_ISR_Handler(USB_OTG_CORE_HANDLE * pdev);
-extern __IO uint32_t data_sent;
+
 /******************************************************************************/
 /*            Cortex-M4 Processor Exceptions Handlers                         */
 /******************************************************************************/

user/stm32f4xx_it.h
@@ -35,7 +35,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
-#include "usbd_cdc_core_loopback.h"
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/